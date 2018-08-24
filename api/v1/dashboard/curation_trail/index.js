c05592d42dde59110ba785800f8f3a722d87f552
Mahdi Yari <m.yary15@gmail.com>
2018-08-24 19:56:16 +0430

    enable/disable trail
    trail will be able to disable/enable their trail

--- api/v1/dashboard/curation_trail/index.js
+++ api/v1/dashboard/curation_trail/index.js
@@ -4,10 +4,12 @@ const followTrail = require('./follow_trail')
 const unfollowTrail = require('./unfollow_trail')
 const settingsTrail = require('./settings_trail')
 const becomeTrail = require('./become_trail')
+const updateTrail = require('./update_trail')
 
 app.use('/follow', followTrail)
 app.use('/unfollow', unfollowTrail)
 app.use('/settings', settingsTrail)
 app.use('/become', becomeTrail)
+app.use('/update', updateTrail)
 
 module.exports = app
