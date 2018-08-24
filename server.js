723f538816575caf9000f52012752c806be03dbf
Mahdi Yari <m.yary15@gmail.com>
2018-08-24 18:52:12 +0430

    server.js - express server listener
    I will use server.js to declare main routes and I will configure sub routes somewhere else

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const hpp = require('hpp')
+const app = express()
+const isAuth = require('./middlewares/is_auth')
+const curationTrail = require('./api/v1/dashboard/curation_trail')
+
+// support json encoded bodies and encoded bodies
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+// more info: www.npmjs.com/package/hpp
+app.use(hpp())
+
+// Check login information for all api calls
+app.use(isAuth)
+app.use('/api/v1/dashboard/curation_trail', curationTrail)
+
+const port = process.env.PORT || 3001
+const host = process.env.HOST || '127.0.0.1'
+app.listen(port, host, () => {
+  console.log(`Application started on ${host}:${port}`)
+})
