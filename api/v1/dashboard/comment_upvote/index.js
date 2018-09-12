a964db616e45fdad80dc6c23aacbea63736a2fee
Mahdi Yari <m.yary15@gmail.com>
2018-09-12 23:39:10 +0430

    add settings comment upvote

--- api/v1/dashboard/comment_upvote/index.js
+++ api/v1/dashboard/comment_upvote/index.js
@@ -3,8 +3,10 @@ const app = express()
 
 const addUser = require('./add_cu')
 const deleteUser = require('./delete_cu')
+const settingsUser = require('./settings_cu')
 
 app.use('/add', addUser)
 app.use('/delete', deleteUser)
+app.use('/settings', settingsUser)
 
 module.exports = app
