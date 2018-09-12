f1e6e16af92ab485e2e19c93271ce129142c54da
Mahdi Yari <m.yary15@gmail.com>
2018-09-12 22:30:04 +0430

    delete user from list

--- api/v1/dashboard/comment_upvote/index.js
+++ api/v1/dashboard/comment_upvote/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 
 const addUser = require('./add_cu')
+const deleteUser = require('./delete_cu')
 
 app.use('/add', addUser)
+app.use('/delete', deleteUser)
 
 module.exports = app
