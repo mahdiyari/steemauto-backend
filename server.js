2cd403485ad74b6bcedd6918d23f40da6e8ed27f
Mahdi Yari <m.yary15@gmail.com>
2018-12-18 20:09:10 +0330

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const commentUpvote = require('./api/v1/dashboard/comment_upvote')
 const claimReward = require('./api/v1/dashboard/claim_reward')
 const loginMethod = require('./api/v1/login')
 const logoutMethod = require('./api/v1/logout')
+const dashboardFront = require('./api/v1.1/dashboard')
+const curationTrailFront = require('./api/v1.1/curation-trail')
 // Processing backend processes (private api)
 const v2Function = require('./api/v2/function')
 
@@ -18,6 +20,12 @@ const v2Function = require('./api/v2/function')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 // more info: www.npmjs.com/package/hpp
 app.use(hpp())
@@ -29,6 +37,9 @@ app.use('/api/v1/dashboard/comment_upvote', commentUpvote)
 app.use('/api/v1/dashboard/claim_reward', claimReward)
 app.use('/api/v1/login', loginMethod)
 app.use('/api/v1/logout', logoutMethod)
+
+app.use('/api/v1.1/dashboard', dashboardFront)
+app.use('/api/v1.1/curation-trail', curationTrailFront)
 
 app.use('/api/v2/function', v2Function)
 
